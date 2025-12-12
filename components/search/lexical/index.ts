9ec4f7e44e88c93576ba6a80b8e2660e30949fe3
Raphael S. <52125687+raphaelsalaja@users.noreply.github.com>
2025-12-12 00:21:10 +0000

    chore: remove unused search and chip node components

--- components/search/lexical/index.ts
+++
@@ -1,3 +0,0 @@
-export { ChipNode, $createChipNode, $isChipNode } from "./chip-node";
-export type { SerializedChipNode } from "./chip-node";
-export { SingleLinePlugin, ChipBackspacePlugin, useEditorChips } from "./plugins";
