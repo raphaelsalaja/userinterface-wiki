bbd93a50e1faa037ce26cd8dc1f1bfd04db3356f
Raphael S. <52125687+raphaelsalaja@users.noreply.github.com>
2025-12-15 22:42:59 +0000

    feat(mdx): update h2 rendering and style adjustments
    - Modify h2 component to render an hr for footnote labels.
    - Adjust hr height and background color for better visibility.
    - Remove unused typography styles to streamline CSS.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -6,7 +6,12 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...components,
     h1: (props) => <h1 data-prose-type="heading" {...props} />,
-    h2: (props) => <h2 data-prose-type="heading" {...props} />,
+    h2: (props) =>
+      props.id === "footnote-label" ? (
+        <hr />
+      ) : (
+        <h2 data-prose-type="heading" {...props} />
+      ),
     h3: (props) => <h3 data-prose-type="heading" {...props} />,
     h4: (props) => <h4 data-prose-type="heading" {...props} />,
     h5: (props) => <h5 data-prose-type="heading" {...props} />,
