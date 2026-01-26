5e676c3f7f8eaa6996675734d617f229b3abc205
Raphael S. <52125687+raphaelsalaja@users.noreply.github.com>
2026-01-26 20:34:27 +0000

    feat: first draft

--- .source/server.ts
+++ .source/server.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
-import * as __fd_glob_4 from "../content/to-spring-or-not-to-spring/index.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/taking-advantage-of-pseudo-elements/index.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/to-spring-or-not-to-spring/index.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/taking-advantage-of-pseudo-elements/index.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/synthesizing-interface-sounds/index.mdx?collection=docs"
 import * as __fd_glob_2 from "../content/sounds-on-the-web/index.mdx?collection=docs"
 import * as __fd_glob_1 from "../content/mastering-animate-presence/index.mdx?collection=docs"
 import * as __fd_glob_0 from "../content/12-principles-of-animation/index.mdx?collection=docs"
@@ -12,4 +13,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content", {}, {"12-principles-of-animation/index.mdx": __fd_glob_0, "mastering-animate-presence/index.mdx": __fd_glob_1, "sounds-on-the-web/index.mdx": __fd_glob_2, "taking-advantage-of-pseudo-elements/index.mdx": __fd_glob_3, "to-spring-or-not-to-spring/index.mdx": __fd_glob_4, });
+export const docs = await create.docs("docs", "content", {}, {"12-principles-of-animation/index.mdx": __fd_glob_0, "mastering-animate-presence/index.mdx": __fd_glob_1, "sounds-on-the-web/index.mdx": __fd_glob_2, "synthesizing-interface-sounds/index.mdx": __fd_glob_3, "taking-advantage-of-pseudo-elements/index.mdx": __fd_glob_4, "to-spring-or-not-to-spring/index.mdx": __fd_glob_5, });
